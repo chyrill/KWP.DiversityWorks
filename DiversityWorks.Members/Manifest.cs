9a00bb6a64fa6953b5a1bfd8f1fac1e899846aa1
chyrill.cabrera@gmail.com <almiks88>
2019-05-14 08:05:37 +0800

    Initial check-in of module KWP.DiversityWorks

---
+++ DiversityWorks.Members/Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "DiversityWorks.Members",
+    Author = "The KWP Diversity Team",
+    Website = "http://kwp.com.au",
+    Version = "1.0.0",
+    Description = "Diversity works memeber module."
+)]
---
+++ DiversityWorks.TheAdmin/Manifest.cs
@@ -0,0 +1,11 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Diversity Admin Theme",
+    Author = "Diversity Works Team",
+    Website = "https://kwp.com.au",
+    Version = "1.0.0",
+    Description = "The diversity works site.",
+    Tags = new[] { "admin" },
+    BaseTheme = "TheAdmin"
+)]
